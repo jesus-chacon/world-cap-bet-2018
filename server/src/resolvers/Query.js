bbf4beadcbc112d4a7f2c167e41dd36aa7f56f2a
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-06-16 15:44:30 +0200

    Add query for countries and players

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -5,6 +5,12 @@ const Query = {
         const id = getUserId(ctx)
         return ctx.db.query.user({where: {id}}, info);
     },
+    players(parent, {search}, ctx, info) {
+        return ctx.db.query.players({where: {name_contains: search}}, info)
+    },
+    countries(parent, {search}, ctx, info) {
+        return ctx.db.query.countries({where: {name_contains: search}}, info)
+    }
 }
 
 module.exports = {Query};
