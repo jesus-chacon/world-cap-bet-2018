8a9a6583e0e5c9da670c9d5749f301d7e27b3479
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-06-16 11:03:31 +0200

    Fix error in authpayload for login and signup

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,10 +1,6 @@
 const {getUserId} = require('../utils')
 
 const Query = {
-    feed(parent, args, ctx, info) {
-        return ctx.db.query.posts({where: {isPublished: true}}, info)
-    },
-
     me(parent, args, ctx, info) {
         const id = getUserId(ctx)
         return ctx.db.query.user({where: {id}}, info);
