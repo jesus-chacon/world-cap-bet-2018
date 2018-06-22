c2fe994e3d39299984189d0015ffda2559802ac2
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-06-22 17:00:35 +0200

    Add main page (static)

---
+++ src/pages/mainPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>MAIN PAGE</h1>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
