111dd3539e410e77bff1eccf27e6b2543ac4ed0f
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-07-12 13:45:26 +0200

    Add mutation to add groups selection to bet

--- server/src/resolvers/Mutation/bet.js
+++ server/src/resolvers/Mutation/bet.js
@@ -4,46 +4,68 @@ const Moment = require('moment');
 
 const {getUserId} = require('../../utils');
 
+async function ensureBet(ctx) {
+    const userId = getUserId(ctx);
+
+    let {bet} = (await ctx.db.query.users({where: {id: userId}}, '{bet { id }}'))[0];
+
+    if (!bet) {
+        const createBet = await ctx.db.mutation.createBet({data: {}}, '{id}');
+
+        await ctx.db.mutation.updateUser({
+            where: {id: userId},
+            data: {bet: {connect: createBet}}
+        }, '{id}');
+
+        bet = createBet.id;
+    }
+
+    return bet;
+}
+
+async function checkListOfCountries(ctx, countries, length) {
+    _.remove(countries, country => !country || country.trim().length == 0);
+    countries = _.uniq(countries);
+
+    if (countries.length != length) throw new Error('The groups selected are invalid');
+
+    let checks = await Q.all(_.map(countries, id => ctx.db.exists.Country({id})));
+    let noValid = _.remove(checks, check => !check);
+
+    if (noValid.length > 0) throw new Error("The countries selected are invalid");
+}
+
+function createCountriesList(ctx, ids, name) {
+    let data = {
+        name,
+        countries: {
+            connect: _.map(ids, id => ({id}))
+        }
+    };
+
+    return ctx.db.mutation.createCountriesList({data}, '{id}');
+}
+
 const bet = {
     async addGroupsToBet(parent, args, ctx, info) {
         const {groups} = args;
-        const userId = getUserId(ctx);
-
-        let {bet} = (await ctx.db.query.users({where: {id: userId}}, '{bet { id }}'))[0];
-
-        if (!bet) {
-            const createBet = await ctx.db.mutation.createBet({data: {}}, '{id}');
-
-            await ctx.db.mutation.updateUser({
-                where: {id: userId},
-                data: {bet: {connect: createBet}}
-            }, '{id}');
-
-            bet = createBet.id;
-        }
-
-        throw new Error();
+        const bet = await ensureBet(ctx);
 
         if (groups.length != 8) throw new Error('The number of groups is wrong');
 
         let selection = [];
-
         _.forEach(groups, group => {selection = _.concat(selection, group)});
-        _.remove(selection, country => !country || country.trim().length == 0);
-        selection = _.uniq(selection);
 
-        if (selection.length != 32) throw new Error('The groups selected are invalid');
+        await checkListOfCountries(ctx, selection, 32);
 
-        let checks = await Q.all(_.map(selection, countryId => ctx.db.exists.Country({id: countryId})));
-        let noValid = _.remove(checks, check => !check);
+        let list = await createCountriesList(ctx, selection, 'groups');
 
-        if (noValid.length > 0) throw new Error("The countries selected are invalid");
-
-        return ctx.db.mutation.createBet({
+        return ctx.db.mutation.updateBet({
+            where: bet,
             data: {
-                created
+                groups: {connect: list}
             }
-        })
+        }, info);
     }
 };
 
