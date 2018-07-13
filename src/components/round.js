bf48815c5541f5a47cf3cbadcfcc84b15505e743
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-07-13 17:37:24 +0200

    Add Round component

---
+++ src/components/round.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+
+import Select from './select';
+
+class Round extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            selectedCountries: []
+        };
+    }
+
+    componentWillMount() {
+        let selectedCountries = _.times(this.props.maximunSelection, _.constant('sampleId'));
+
+        if (!!this.props.previousSelection && this.props.previousSelection.length > 0) {
+            this.props.previousSelection.forEach((id, index) => {
+                selectedCountries[index] = id;
+            });
+        }
+
+        console.log(selectedCountries);
+        this.setState({selectedCountries});
+    }
+
+    render() {
+        let selectedCountries = this.state.selectedCountries;
+
+        return (
+            <div className="round">
+                <div className="round__title">
+                    {this.props.title}
+                </div>
+
+                <div className="round__body form form-inline">
+                    {
+                        selectedCountries.map((id, index) => (
+                            <div className="form-group align-items-center" key={index}>
+                                <label>{index + 1}.</label>
+
+                                <Select
+                                    index={index}
+                                    value={id}
+                                    onChange={this._handleChangeCountry.bind(this)}
+                                    options={this.props.options}
+                                />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        );
+    }
+
+    _handleChangeCountry(index, countryId) {
+        let selectedCountries = this.state.selectedCountries;
+
+        selectedCountries[index] = countryId;
+
+        this.setState({selectedCountries});
+
+        this.props.handleChangeCountry(selectedCountries);
+    }
+};
+
+export default Round;
